1bce2f1247d1f2fe1936dacfd3c6ee6806a3912d
Nikhil CSB <csbnikhil@gmail.com>
2020-05-14 03:39:31 +0530

    basics done

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -6,15 +6,36 @@
 
 #include <stdio.h>
 
+char *VERSES[] = {
+    "3Fizz",
+    "5Buzz"
+};
+
+void fizzbuzz(int num)
+{
+    int flag = 0;
+
+    if (num % 3 == 0)
+    {
+        printf("Fizz");
+        flag++;
+    }
+
+    if (num % 5 == 0)
+    {
+        printf("Buzz");
+        flag++;
+    }
+
+    if (!flag)
+        printf("%d", num);
+
+    printf("\n");
+}
+
 int main()
 {
-    char *verses[] = {
-        NULL,
-        NULL,
-        NULL,
-        "Fizz",
-        NULL,
-        "Buzz
-    };
+    for (int i = 0; i < 100; ++i)
+        fizzbuzz(i);
 }
 
