6fa464d133ce42c282d1741b0284657af2bdf936
Nikhil CSB <csbnikhil@gmail.com>
2020-05-17 20:13:43 +0530

    add file

---
+++ ord.c
@@ -0,0 +1,23 @@
+/********************************\
+ *                              *
+ *   Return ASCII Number        *
+ *   of a characer.             *
+ *                              *
+ *   Author: @theteachr         *
+\********************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s [character]\n", argv[0]);
+        return -1;
+    }
+
+    printf("ord('%c') = %u\n", argv[1][0], argv[1][0]);
+
+    return 0;
+}
+
