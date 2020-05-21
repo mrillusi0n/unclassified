1c877fc3feec5abe1ab03304f23df7c6e971998f
Nikhil CSB <csbnikhil@gmail.com>
2020-05-21 22:16:57 +0530

    modularize

--- concentric_numbers.c
+++ concentric_numbers.c
@@ -10,6 +10,16 @@
  *          3 2 2 2 3           *
  *          3 3 3 3 3           *
  *                              *
+ *                              *
+ *        4 4 4 4 4 4 4         *
+ *        4 3 3 3 3 3 4         *
+ *        4 3 2 2 2 3 4         *
+ *        4 3 2 1 2 3 4         *
+ *        4 3 2 2 2 3 4         *
+ *        4 3 3 3 3 3 4         *
+ *        4 4 4 4 4 4 4         *
+ *                              *
+ *                              *
  * Author: @theteachr           *
 \********************************/
 
@@ -24,20 +34,27 @@ void printer(int outer, int inner) {
 	while (inner < outer) printf("%d ", ++inner);
 }
 
+void concentricize(int size)
+{
+    int i;
+
+	for (i = size; i > 0; --i) {
+		printer(size, i);
+		printf("\n");
+	}
+
+	for (i = 2; i <= size; ++i) {
+		printer(size, i);
+		printf("\n");
+	}
+}
+
 int main() {
 
-	int n = 6;
+	int n = 4;
 	int i;
 	
-	for (i = n; i > 0; --i) {
-		printer(n, i);
-		printf("\n");
-	}
-
-	for (i = 2; i <= n; ++i) {
-		printer(n, i);
-		printf("\n");
-	}
+    concentricize(n);
 	
 	return 0;
 }
