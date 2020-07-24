89bf6541f5c022da0b6f2f690b2b1fc726fd978a
Nikhil CSB <csbnikhil@gmail.com>
2020-07-24 14:58:52 +0530

    improve modularity

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -8,22 +8,22 @@
 
 char *VERSES[] = {
     "3Fizz",
-    "4Buzz",
-    "5Boop"
+    "5Buzz",
+    "7Boop"
 };
 
 int NUM_VERSES = 3;
 
-void fizzbuzz(int num)
+void fizzbuzz(int num, char *verses[], int n_verses)
 {
     int flag = 0;
 
-    for (int i = 0; i < NUM_VERSES; ++i)
+    for (int i = 0; i < n_verses; i++)
     {
         int div;
         char msg[5];
 
-        sscanf(VERSES[i], "%d%s", &div, msg);
+        sscanf(verses[i], "%d%s", &div, msg);
 
         if (num % div == 0)
         {
@@ -41,6 +41,5 @@ void fizzbuzz(int num)
 int main()
 {
     for (int i = 0; i < 40; ++i)
-        fizzbuzz(i);
+        fizzbuzz(i, VERSES, 3);
 }
-
