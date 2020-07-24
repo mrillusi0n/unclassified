7838284426fa0b9d649e7abe25cb0ae821c00135
Nikhil CSB <csbnikhil@gmail.com>
2020-07-24 23:03:39 +0530

    improve formatting

--- duplicates/distinctifier.c
+++ duplicates/distinctifier.c
@@ -52,11 +52,12 @@ void swap(int *a, int *b) {
 
 int distinctify(int *arr, int size) {
     int j, i;
+    
     for (i = 1, j = 0; i < size; ++i) {
         if (arr[j] == arr[i])
             continue;
         swap(arr + ++j, arr + i);
     }
+
     return j + 1;
 }
-
