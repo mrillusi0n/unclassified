b7e483aa45671ec3517a1a6aaf38457f04a3a7e3
Nikhil CSB <csbnikhil@gmail.com>
2020-08-10 22:21:39 +0530

    enums and structs

---
+++ kemm.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef enum {
+    NEGATIVE = -1,
+    NOT_TESTED,
+    POSITIVE,
+} covid_status;
+
+typedef struct {
+    char *name;
+    char *id;
+    covid_status status;
+} person;
+
+covid_status covid_test(person *p) {
+    // some logic that performs the test
+    // and determines whether the person is infected
+
+    return NEGATIVE;
+}
+
+int main(int argc, char const *argv[])
+{
+    person harry = {
+        .name = "Harry P.",
+        .id = "HP019823",
+        .status = NOT_TESTED,
+    };
+
+    switch (covid_test(&harry)) {
+        case POSITIVE: printf("Unsafe.\n"); break;
+        case NEGATIVE: printf("Safe.\n"); break;
+        case NOT_TESTED: printf("Not safe, yet.\n"); break;
+    }
+
+    return 0;
+}
